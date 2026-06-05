65e621e34c10eb7f1fdf189fb269e73d05835690
Krupal015 <patelkrupald2324@gmail.com>
2026-06-06 03:58:01 +0530

    react-demo files

--- 09-useState/src/App.jsx
+++ 09-useState/src/App.jsx
@@ -2,19 +2,35 @@ import React, { useState } from 'react'
 
 const App = () => {
 
- const [counter, setCounter] = useState(0)
+//  const [counter, setCounter] = useState(0)
 
- function setCountI(){
-  setCounter(counter+1)
- }
- function setCountD(){
-setCounter(counter-1)
- }
+//  function setCountI(){
+//   setCounter(counter+1)
+//  }
+//  function setCountD(){
+// setCounter(counter-1)
+//  }
+
+ const [num,setNum] = useState({username:'sarthak',age:20})
+
+function btnClicked(){
+const newNum = {...num}
+newNum.username = "krupal"
+newNum.age=21;
+setNum(newNum)
+}
+
   return (
     <div>
-      <h1>Counter= {counter}</h1>
+      {/* <h1>Counter= {counter}</h1>
       <button onClick={setCountI}>Increase</button>
-      <button onClick={setCountD}>Decrease</button>
+      <button onClick={setCountD}>Decrease</button> */}
+
+        
+          <h1>{num.username}{num.age}</h1>
+          <button onClick={btnClicked}>change</button>
+          
+        
     </div>
 
 
---
+++ 12-notes-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <div>
+    <form  className='flex item-center'>
+      <input type="text" placeholder='Enter Note heading' className='px-' />
+      <input type="text" placeholder='Enter details ' />
+    </form>
+    </div>
+  )
+}
+
+export default App
